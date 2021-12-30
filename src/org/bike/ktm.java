c3c9c731bbda6dfd50b520fdbbe7df3a7459f45d
JeganSundharam <sundharam.jegan@gmail.com>
2021-12-30 18:22:51 +0530

    First time uploading

---
+++ src/org/bike/ktm.java
@@ -0,0 +1,23 @@
+package org.bike;
+
+public class ktm implements Bike  {
+
+	
+	public void cost() {
+		System.out.println("Cost of the bike : Rs.1,50,000");
+		
+	}
+
+	
+	public void speed() {
+		System.out.println("Top Speed of the bike : 200KmpH");
+		
+			}
+	public static void main (String[] args) {
+		ktm k=new ktm();
+		k.cost();
+		k.speed();
+		
+	}
+
+}
